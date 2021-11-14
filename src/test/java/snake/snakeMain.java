0cbbb1d055267923b1e87d4e31a9db69f8a19102
whatshu <1355992759@qq.com>
2021-11-14 20:36:01 +0800

    change:   update snake

--- src/test/java/snake/snakeMain.java
+++ src/test/java/snake/snakeMain.java
@@ -8,11 +8,14 @@ public class snakeMain {
 
     public static void main(String[] args) {
         geCore core = new geCore("snake-core");
+        core.setSize(1280, 960);
         try {
             core.loadResource("ball", "resources/ball.png");
         } catch (geException ignored) {}
 
+        core.addLayer("ball-layer", "ball", 0, -1, 1, 1, 1);
 
+        core.update();
     }
 
 }
